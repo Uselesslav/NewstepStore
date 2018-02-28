bb0f6614bce27e960660ab95f49a21d150babb6c
Uselesslav <wdbondarenko@gmail.com>
2018-02-28 16:47:06 +0300

    Добавлены основные поля в модели

--- app/src/main/java/store/newstep/newstepstore/model/Product.kt
+++ app/src/main/java/store/newstep/newstepstore/model/Product.kt
@@ -1,7 +1,14 @@
 package store.newstep.newstepstore.model
 
+import android.graphics.Bitmap
+
 /**
  * Created by wyacheslav on 27.02.18.
  * Товар
  */
-class Product
+class Product(
+        name: String,
+        price: Int,
+        category: Category,
+        bitmap: Bitmap
+)
--- app/src/main/java/store/newstep/newstepstore/model/Sentence.kt
+++ app/src/main/java/store/newstep/newstepstore/model/Sentence.kt
@@ -1,7 +1,12 @@
 package store.newstep.newstepstore.model
 
+import android.graphics.Bitmap
+
 /**
  * Created by wyacheslav on 27.02.18.
  * Предложение
  */
-class Sentence
+class Sentence(
+        title: String = "title",
+        description: String = "description",
+        bitmap: Bitmap)
